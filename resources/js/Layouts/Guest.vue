600a040c575b490013c30ec773cfa624fcf68a92
Alemhar <alemhar@gmail.com>
2021-11-20 20:07:35 +0800

    initial commit

---
+++ resources/js/Layouts/Guest.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div>
+            <Link href="/">
+                <img src="img/kuwagoo_logo_400x400.png" style="width: 200px; height:auto;"/>
+                
+            </Link>
+        </div>
+
+        <div class="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+import BreezeApplicationLogo from '@/Components/ApplicationLogo.vue'
+import { Link } from '@inertiajs/inertia-vue3';
+
+export default {
+    components: {
+        Link,
+    }
+}
+</script>
